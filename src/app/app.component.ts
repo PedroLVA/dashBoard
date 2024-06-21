c1551877826ca0e72588f8d1050c6ce39b9f04d2
Pedro Athayde <pedro_athayde@outlook.com>
2024-06-21 17:06:50 -0300

    content projection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,12 +4,13 @@ import { HeaderComponent } from "./components/header/header.component";
 import { ServerComponent } from "./components/dashboard/server/server.component";
 import { TrafficComponent } from "./components/dashboard/traffic/traffic.component";
 import { TicketsComponent } from "./components/dashboard/tickets/tickets.component";
+import { DashboardItemComponent } from "./components/dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
-    imports: [ServerComponent, TrafficComponent, TicketsComponent, HeaderComponent]
+    imports: [ServerComponent, TrafficComponent, TicketsComponent, HeaderComponent, DashboardItemComponent]
 })
 export class AppComponent {
  
