71abbd0d5e30dac83291150d7f353f326d2039e4
Pedro Athayde <pedro_athayde@outlook.com>
2024-06-26 17:38:34 -0300

    Ticket being created, ticket interface created

--- src/app/components/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/components/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,7 +1,8 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild, viewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, OnInit, Output, ViewChild, output, viewChild } from '@angular/core';
 import { ControlComponent } from "../../../../shared/control/control.component";
 import { ButtonComponent } from '../../../../shared/button/button.component';
 import { FormsModule } from '@angular/forms';
+import { Ticket } from '../ticket/ticket.model';
 
 @Component({
     selector: 'app-new-ticket',
@@ -10,16 +11,17 @@ import { FormsModule } from '@angular/forms';
     styleUrl: './new-ticket.component.css',
     imports: [ControlComponent, ButtonComponent, FormsModule]
 })
-export class NewTicketComponent implements OnInit, AfterViewInit {
+export class NewTicketComponent implements  AfterViewInit {
 
     @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+    @Output() add = new EventEmitter<{title: string, text: string}>();
+
+    //add1 = output<{title: string, text: string}>();
     //private formChild = viewChild<ElementRef<HTMLFormElement>>('form');
 
-    ngOnInit() {
-        
-    }
 
     onSubmit(titleInput: string, textInput: string){
+       this.add.emit({title: titleInput, text: textInput});
        
         console.dir(titleInput + ' ' + textInput);
         this.form?.nativeElement.reset();
--- src/app/components/dashboard/tickets/tickets.component.ts
+++ src/app/components/dashboard/tickets/tickets.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 import { NewTicketComponent } from "./new-ticket/new-ticket.component";
+import { Ticket } from './ticket/ticket.model';
+import { TicketComponent } from "./ticket/ticket.component";
 
 @Component({
     selector: 'app-tickets',
     standalone: true,
     templateUrl: './tickets.component.html',
     styleUrl: './tickets.component.css',
-    imports: [NewTicketComponent]
+    imports: [NewTicketComponent, TicketComponent]
 })
 export class TicketsComponent {
+
+    tickets: Ticket[] = [];
+
+    onAdd(ticketData: { title: string, text: string }) {
+        this.tickets.push({
+            id: Math.random().toString(),
+            title: ticketData.title,
+            request: ticketData.text,
+            status: 'open',
+        })
+    }
 
 }
